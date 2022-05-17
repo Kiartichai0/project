73f0504d42b6051677da1442ae84cf303c77163d
Kiartichai0 <kiartichai.su.61@ubu.ac.th>
2022-05-17 20:40:41 +0700

    build

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,9 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/project/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -57,8 +60,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:3000/api',
-    //baseURL: 'http://localhost:2025/api',
+    baseURL: '/project/api',
   },
   auth: {
     strategies: {
