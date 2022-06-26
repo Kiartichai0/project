5c432b1169ccfa9c45874ef12f06c79efff34de4
Kiartichai0 <kiartichai.su.61@ubu.ac.th>
2022-06-26 16:58:03 +0700

    delete lock file

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   target: 'static',
 
   router: {
-    base: ''
+    base: '/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
@@ -61,7 +61,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:3000/api',
+    baseURL: '/api',
   },
   
   auth: {
